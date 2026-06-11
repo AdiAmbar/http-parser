a7f864d0340216750630484d171fcd586beba98d
adiimbar <adiimbar@users.noreply.github.com>
2026-06-11 14:05:39 +0300

    refactor(headers): add header key token validation

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,17 +3,52 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
-
-type Headers map[string]string
 
 var crlf = []byte("\r\n")
 
 var ERROR_BAD_HEADER_LINE = fmt.Errorf("bad header line")
 var ERROR_MALFORMED_FIELD_NAME = fmt.Errorf("malformed field name")
+var ERROR_MALFORMED_HEADER_NAME = fmt.Errorf("malformed header name")
+var ERROR_HEADER_KEY_CONTAINS_INVALID_CARACTERS = fmt.Errorf("header key contains invalid characters")
 
-func NewHeaders() Headers {
-	return make(Headers)
+type Headers struct {
+	headers map[string]string
+}
+
+func NewHeaders() *Headers {
+	return &Headers{
+		headers: map[string]string{},
+	}
+}
+
+func (h *Headers) Get(name string) string {
+	return h.headers[strings.ToLower(name)]
+}
+
+func (h *Headers) Set(name, value string) {
+	h.headers[strings.ToLower(name)] = value
+}
+
+func isValidToken(str []byte) bool {
+	for _, ch := range str {
+		found := false
+		if ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9' {
+			found = true
+		}
+
+		switch ch {
+		case '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+			found = true
+		}
+
+		if !found {
+			return false
+		}
+	}
+
+	return true
 }
 
 func parseHeaderLine(fieldLine []byte) (string, string, error) {
@@ -29,10 +64,12 @@ func parseHeaderLine(fieldLine []byte) (string, string, error) {
 		return "", "", ERROR_MALFORMED_FIELD_NAME
 	}
 
-	return string(fieldName), string(fieldValue), nil
+	fieldNameLower := strings.ToLower(string(fieldName))
+
+	return fieldNameLower, string(fieldValue), nil
 }
 
-func (h Headers) Parse(data []byte) (int, bool, error) {
+func (h *Headers) Parse(data []byte) (int, bool, error) {
 
 	read := 0
 	done := false
@@ -48,13 +85,17 @@ func (h Headers) Parse(data []byte) (int, bool, error) {
 			break
 		}
 
-		fieldName, fieldValue, err := parseHeaderLine(data[read:read+idx])
+		fieldName, fieldValue, err := parseHeaderLine(data[read : read+idx])
 		if err != nil {
 			return 0, false, err
 		}
 
+		if !isValidToken([]byte(fieldName)) {
+			return 0, false, ERROR_MALFORMED_HEADER_NAME
+		}
+
 		read += idx + len(crlf)
-		h[fieldName] = fieldValue
+		h.Set(fieldName, fieldValue)
 	}
 
 	return read, done, nil
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -15,9 +15,9 @@ func TestHeadersParse(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
-	assert.Equal(t, "barbar", headers["FooFoo"])
-	assert.Equal(t, "", headers["MissingKey"])
+	assert.Equal(t, "localhost:42069", headers.Get("Host"))
+	assert.Equal(t, "barbar", headers.Get("FooFoo"))
+	assert.Equal(t, "", headers.Get("MissingKey"))
 	assert.Equal(t, 47, n)
 	assert.True(t, done)
 
