573f68132c49c6b2f5d95820016a59f8e3146a16
adiimbar <adiimbar@users.noreply.github.com>
2026-06-11 17:34:22 +0300

    refactor(headers): handle multiple values with the same key

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -36,4 +36,13 @@ func TestHeadersParse(t *testing.T) {
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
+
+	// Test: Multiple header values with same key
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069\r\nHost: example.com\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	assert.Equal(t, "localhost:42069, example.com", headers.Get("Host"))
+	// assert.Equal(t, 49, n)
+	assert.True(t, done)
 }
